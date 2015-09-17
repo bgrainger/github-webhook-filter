04f9786da4f418c0b72bc85766b6e92319ecfd2b
Bradley Grainger <bgrainger@gmail.com>
2015-09-17 09:47:46 -0700

    Add initial version.

---
+++ README.md
@@ -0,0 +1,41 @@
+# GitHub Webhook Filter
+
+`github-webhook-filter` is a GitHub webhook that forwards webhook events to an arbitrary target,
+unless the event type or data is blocked.
+
+This is useful for sending most `push` events to Flowdock except ones that happen on a specific
+branch.
+
+## How to Use
+
+### GitHub Enterprise
+
+At https://git/Logos/YourRepoName/settings, choose "Webhooks and Services".
+
+Click "Add Webhook":
+* Payload URL: http://github-webhook-filter.lrscorp.net:4001/github?target=url-encoded-target-uri
+* Content type: application/json
+* Secret: (blank)
+* Send me **everything**
+
+Click "Add webhook"
+
+## Requirements
+
+* [GitHub Enterprise](https://enterprise.github.com/)
+* [NodeJS](https://nodejs.org/)
+
+## Installation
+
+### Server
+
+Apply the `github-webhook-filter_server` puppet class to the node and run
+`puppet apply -t`.
+
+To restart the service, run `svcadm restart github-webhook-filter`.
+
+### Deployment
+
+Add a "deploy" git remote: `git remote add deploy ssh://desk-dev-util01.lrscorp.net/usr/local/src/github-webhook-filter.git`
+
+To deploy the code: `git push deploy master`
---
+++ index.js
@@ -0,0 +1,97 @@
+var http = require('http');
+var https = require('https');
+var url = require('url');
+
+// drop all events except these ones by default
+var eventWhiteList = {
+	'commit_comment': true,
+	'fork': true,
+	'issue_comment': true,
+	'issues': true,
+	'pull_request': true,
+	'pull_request_review_comment': true,
+	'push': true
+};
+
+// Returns 'true' if the specified GitHub event type (https://developer.github.com/v3/activity/events/types/)
+// should be blocked.
+function blockEventType(gitHubEventType) {
+	return eventWhiteList[gitHubEventType];
+}
+
+// Returns 'true' if the specified event should be blocked.
+function blockEvent(gitHubEventType, eventData) {
+	return gitHubEventType === 'push' && eventData.ref.substring(0, 15) === 'refs/heads/lprb';
+}
+
+// Converts 'rawHeaders' (an array of strings) into a hash of header->value. The return
+// value is similar to request.headers except that the original header casing is preserved.
+function getHeaders(rawHeaders) {
+	var headers = {};
+	for (var i = 0; i < rawHeaders.length; i += 2) {
+		headers[rawHeaders[i]] = rawHeaders[i + 1];
+	}
+	return headers;
+}
+
+var server = http.createServer(function(request, response) {
+	// get 'target' query parameter
+	var parsedRequestUrl = url.parse(request.url, true);
+	var targetUrl = parsedRequestUrl.query.target;
+	console.log('got request for ' + targetUrl);
+	var parsedTargetUrl = url.parse(targetUrl);
+
+	// save the POSTed data
+	var requestBuffers = [];
+	request.on('data', function(chunk) {
+		requestBuffers.push(chunk);
+	});
+
+	request.on('end', function() {
+		try {
+			// parse the JSON request
+			var requestData = Buffer.concat(requestBuffers);
+			var requestJson = JSON.parse(requestData.toString());
+
+			// check if request should be filtered
+			var gitHubEventType = request.headers['x-github-event'];
+			if (blockEventType(gitHubEventType) || blockEvent(gitHubEventType, requestJson)) {
+				// drop this request
+				response.statusCode = 200;
+				response.end('Event dropped by github-webhook-filter.');
+				return;
+			}
+
+			// build request to specified target
+			var isHttp = parsedTargetUrl.protocol === 'http:';
+			var targetRequestParams = {
+				protocol: parsedTargetUrl.protocol,
+				hostname: parsedTargetUrl.hostname,
+				port: parsedTargetUrl.port || (isHttp ? 80 : 443),
+				method: request.method,
+				path: parsedTargetUrl.path,
+				headers: getHeaders(request.rawHeaders),
+				agent: false
+			};
+			targetRequestParams.headers.Host = parsedTargetUrl.host;
+			console.log('Making request to ' + targetRequestParams.hostname);
+			var targetRequest = (isHttp ? http : https).request(targetRequestParams, function(targetResponse) {
+				// send target's response back to GitHub
+				response.writeHead(targetResponse.statusCode, targetResponse.statusMessage, getHeaders(targetResponse.rawHeaders));
+				targetResponse.pipe(response);
+			});
+
+			// post received JSON to the target
+			targetRequest.write(requestData);
+			targetRequest.end();			
+		} catch(e) {
+			console.error('ERROR: ' + e);
+			console.error(e.stack);
+			response.statusCode = 500;
+			response.end();
+		}
+	});
+});
+
+console.log('Server listening on port ' + process.env.PORT);
+server.listen(process.env.PORT);
